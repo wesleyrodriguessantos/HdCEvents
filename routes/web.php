865e1d1ad389f773849043be0c62428147e25c6c
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2021-01-07 16:12:00 -0300

    Criando um dashboard para o projeto

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,4 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
