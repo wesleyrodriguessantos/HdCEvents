1f80620f267ff3d095f469bbe40b22a8cb65655f
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2021-01-07 14:24:11 -0300

    Relações

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
