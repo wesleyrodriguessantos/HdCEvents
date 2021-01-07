7f01ec71f054cae9e1a86a7c893679dc7a80744e
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2021-01-07 10:16:49 -0300

    Autenticação - Login e Registro

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
